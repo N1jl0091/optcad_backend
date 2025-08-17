93d7a200fd8c0627c01306b963f8679d8a8c95f4
Nikolas Lowther <nikolaslowther@Nikolass-MacBook-Pro-18778.local>
2025-08-17 12:44:53 +0200

    update

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ SESSIONS = {}
 
 STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
 STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
-REDIRECT_URI = "https://optcadbackend-production.up.railway.app/callback"
+REDIRECT_URI = "optcadbackend-production.up.railway.app"
